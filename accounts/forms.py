b5a11fecb01e221e891aaa151247f6f75e2c3406
Muhammad Jamal Raja <168621123+RajaJamal913@users.noreply.github.com>
2024-11-05 15:34:14 +0500

    Web Streaming App

---
+++ accounts/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'username', 'email', 'password']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            self.add_error("confirm_password", "Passwords do not match.")
---
+++ accounts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+def register_view(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            messages.success(request, "Registration successful. Please log in.")
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'templates/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')  # Change 'dashboard' to your dashboard URL name
+        else:
+            messages.error(request, "Invalid credentials.")
+    return render(request, 'templates/login.html')
+
+@login_required
+def dashboard_view(request):
+    return render (request, ('templates/dashboard.html'))
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
